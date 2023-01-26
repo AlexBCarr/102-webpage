b4af56c4c24ab9eec9bdcdc5f0c544d26ba51dfd
Alex B Carr <mralex.carr@yahoo.com>
2023-01-26 11:47:23 -0800

    Updated Javascript

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ alert("Welcome To Good Burger, Home Of The Good Burger!");
 let usersName = prompt("What is your name?");
 document.write("Hello" + usersName);
 
+let userInput = prompt("How do you make a hamburger smile?");
+console.log(userInput);
+
+let myAnswer = "Pickle it gently";
+console.log(myAnswer);
+
+if (userInput == myAnswer){
+    document.write("HAHAHA Gotta Love Dad Jokes");
+} else if (userInput > myAnswer){
+    document.write("Alright you beat it out of me...YOU PICKLE IT GENTLY!");
+} else if (userInput < myAnswer){
+    document.write("Alright you beat it out of me...YOU PICKLE IT GENTLY!");
+} else{
+    document.write("Youa a comedian also");
+}
+    
+
+
+
+
+
+
 
 
 

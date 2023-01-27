25a37b5c434b2cdf5775d6890aedf378732f30c9
Alex B Carr <mralex.carr@yahoo.com>
2023-01-26 22:52:32 -0800

    Adding Function

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 
 alert("Welcome To Good Burger, Home Of The Good Burger!");
 
-
-let usersName = prompt("What is your name?");
-document.write("Hello" + usersName);
-
-let userInput = prompt("How do you make a hamburger smile?");
-console.log(userInput);
-
-let myAnswer = "Pickle it gently";
-console.log(myAnswer);
-
-if (userInput == myAnswer){
-    document.write("HAHAHA Gotta Love Dad Jokes");
-} else if (userInput > myAnswer){
-    document.write("Alright you beat it out of me...YOU PICKLE IT GENTLY!");
-} else if (userInput < myAnswer){
-    document.write("Alright you beat it out of me...YOU PICKLE IT GENTLY!");
-} else{
-    document.write("Youa a comedian also");
+function sayHello(){
+  let usersName = prompt("What is your name?");
+  console. log("The user's name is:" + usersName);
+  document.write("Hello " + usersName);
+  if (usersName){
+    document.write("   ,wow " + usersName + " is a great name!");
+    return
+  } else {
+    document.write(" Names are fundamental when meeting new people!");
+  }
+  return usersName
 }
+
+
+function dadJoke(){
+  let userInput = prompt("How do you make a hamburger smile?");
+  console.log(userInput);
+
+  let myAnswer = "Pickle it gently";
+  console.log(myAnswer);
+
+  if (userInput == myAnswer){
+    document.write("HAHAHA Gotta Love Dad Jokes");
+  } else if (userInput > myAnswer){
+    document.write("Alright you beat it out of me...YOU PICKLE IT GENTLY!");
+  } else if (userInput < myAnswer){
+    document.write("Alright you beat it out of me...YOU PICKLE IT GENTLY!");
+  } else{
+    document.write("Your a comedian also");
+  }
+  return userInput; 
+
+}
+
+
+
+
+
+
+
     
 
 
